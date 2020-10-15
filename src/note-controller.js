c0e06913c2d5485ee66e1116c43b91d4b60ec02f
Connor Worthington <worthington.connor@gmail.com>
2020-10-15 15:31:11 +0100

    Displaying individual notes
    adds functionality to select note and view full text without page refresh

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,10 +1,6 @@
-// var myDiv = document.getElementById("app")
-// console.log(myDiv)
-// myDiv.innerHTML = "howdy"
 class noteController {
   constructor(listModel) {
     this.listmodel = listModel
-    this.listmodel.addNote("Favourite drink: seltzer")
     this.noteview = new noteListView(this.listmodel)
   }
 
@@ -18,8 +14,66 @@ class noteController {
     noteHtml.innerHTML = this.getHtml();
   }
 
+//   displayChange() {
+  
+//     let notecontroller = this;
+//     listenForUrlChange();
+
+//   function listenForUrlChange() {
+//     window.addEventListener('hashchange', showNewPage);
+//   }
+
+//   function showNewPage(){
+//     getNote(getNoteFromURL(window.location));
+//   }
+
+//   function getNoteFromURL(location) {
+//     return location.hash.split('#')[1];
+//   }
+
+//   function getNote(noteurl) {
+//     let id = noteurl.substring(noteurl.indexOf("/") + 1);
+//     console.log(id)
+//     let note = notecontroller.noteview.noteList.notes[id];
+//     let list = document.getElementById('notes')
+//     list.innerHTML = notecontroller.noteview.output();
+//     let singlenoteview = new singleNoteView(note);
+//     let div = document.getElementById('app')
+//     div.insertHtml = singlenoteview.htmlify();
+//   }
+// }
+    controller = this 
+  showText() {
+    console.log(controller.listmodel.notes[1].text)
+    let text = controller.listmodel.notes[location.hash.split('#')[1]].text;
+    document.getElementById('app').innerHTML = text; 
+  }
+
+  urlChange(){
+    window.addEventListener("hashchange", this.showText);
+  }
+   
+  // makeURLChange() {
+  //   window.addEventListener("hashchange", this.showText);
+  // }
+  // showText() {
+  //   getNote(getNoteFromURL(window.location))
+  // }
+  // getNoteFromURL(location) {
+  //   return location.hash.split('#')[1];
+  // }
+  // getNote(note) {
+  //   document.getElementById('app').innerHTML = note
+  // }
 }
 
 var listModel = new noteListModel
 var controller = new noteController(listModel)
+listModel.addNote("This is a note")
+listModel.addNote("This is another note")
+listModel.addNote("This is another really long note and I should not be able to see the end of it")
 controller.insertHtml();
+controller.urlChange();
+
+
+
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -25,7 +25,7 @@ class noteListView {
       //   noteTwenty.pop()
       // }
 
-    htmlString += '<li><div><a href="#notes/' + notes[index].id.toString() +'">' + notes[index].text.substring(0,20) + "</a></div></li>"
+    htmlString += '<li><div><a href="#' + notes[index].id.toString() +'">' + notes[index].text.substring(0,20) + "</a></div></li>"
     console.log(htmlString)
     if(index == notes.length-1) 
       { htmlString += "</ul>" }
